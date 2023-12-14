d99a68e8fac7a87664dd5c504ce77ac390f3e9ac
asarlykova <150665283+asarlykova@users.noreply.github.com>
2023-12-14 10:37:30 +0600

    Add files via upload

--- battle.py
+++ battle.py
@@ -1,63 +1,48 @@
 print('What is your name?')
 a = input()
-from random import randint
+from random import randint, choice
 
 board_size = 7
 
 def number_to_letter(number):
     return chr(ord('A') + number)
 
-board = []
-
-for _ in range(board_size):
-    board.append(["O"] * board_size)
-
 def print_board(board):
     print("  " + " ".join([number_to_letter(i) for i in range(board_size)]))
     for i, row in enumerate(board):
         print(str(i + 1) + " " + " ".join(row))
 
-print(a, ", let's play Battleship!")
-print_board(board)
-
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-
-for turn in range(12):
-    print("\nTurn", turn)
-    guess_row = int(input("Guess Row (1-" + str(board_size) + "):")) - 1
-    
-    # Validate column input
-    valid_columns = [number_to_letter(i) for i in range(board_size)]
-    guess_col = input("Guess Col (A-" + valid_columns[-1] + "):").upper()
-    
-    while guess_col not in valid_columns:
-        print("Invalid column input. Please enter a valid column letter.")
-        guess_col = input("Guess Col (A-" + valid_columns[-1] + "):").upper()
-
-    guess_col = valid_columns.index(guess_col)
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship!")
-        board[guess_row][guess_col] = "X"
-        break
-    else:
-        if (guess_row < 0 or guess_row >= board_size) or (guess_col < 0 or guess_col >= board_size):
-            print("Oops, that's not even in the ocean.")
-        elif board[guess_row][guess_col] == "X":
-            print("You guessed that one already.")
+def place_ship(board, ship_size):
+    while True:
+        orientation = choice(['horizontal', 'vertical'])
+        if orientation == 'horizontal':
+            ship_row = randint(0, board_size - 1)
+            ship_col = randint(0, board_size - ship_size)
+            if all(board[ship_row][col] == 'O' for col in range(ship_col, ship_col + ship_size)):
+                for col in range(ship_col, ship_col + ship_size):
+                    board[ship_row][col] = 'S'
+                break
         else:
-            print("You missed my battleship!")
-            board[guess_row][guess_col] = "-"
-        
-        # Print the board after each turn
-        print_board(board)
+            ship_row = randint(0, board_size - ship_size)
+            ship_col = randint(0, board_size - 1)
+            if all(board[row][ship_col] == 'O' for row in range(ship_row, ship_row + ship_size)):
+                for row in range(ship_row, ship_row + ship_size):
+                    board[row][ship_col] = 'S'
+                break
 
-    if turn == 11:
-        print("Game Over")
+board = []
+
+for _ in range(board_size):
+    board.append(['O'] * board_size)
+
+print(a, ", let's play Battleship!")
+
+place_ship(board, 3)  
+place_ship(board, 2)  
+place_ship(board, 2) 
+place_ship(board, 1)  
+place_ship(board, 1)
+place_ship(board, 1)
+place_ship(board, 1)
+
+print_board(board)
