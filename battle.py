67fc998c9f557edb73f60793d001b1a08d3b1f3b
asarlykova <150665283+asarlykova@users.noreply.github.com>
2023-12-14 08:51:20 +0600

    Add files via upload

--- battle.py
+++ battle.py
@@ -1,5 +1,5 @@
 print('What is your name?')
-a=input()
+a = input()
 from random import randint
 
 board = []
@@ -11,11 +11,12 @@ def print_board(board):
     for row in board:
         print((" ").join(row))
 
-print(a,"let's play Battleship!")
+print(a, "let's play Battleship!")
 print_board(board)
 
 def random_row(board):
     return randint(0, len(board) - 1)
+
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
@@ -23,23 +24,22 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 
 for turn in range(9):
-    print ("Turn"), turn
+    print("Turn", turn)
     guess_row = int(input("Guess Row:"))
-    guess_col = int(
-        input("Guess Col:"))
+    guess_col = int(input("Guess Col:"))
 
     if guess_row == ship_row and guess_col == ship_col:
         print("Congratulations! You sunk my battleship!")
         break
     else:
-        if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
+        if (guess_row < 0 or guess_row > 7) or (guess_col < 0 or guess_col > 7):
             print("Oops, that's not even in the ocean.")
-        elif(board[guess_row][guess_col] == "X"):
+        elif board[guess_row][guess_col] == "X":
             print("You guessed that one already.")
         else:
             print("You missed my battleship!")
             board[guess_row][guess_col] = "X"
     if turn == 8:
         print("Game Over")
-    turn =+ 1
+    turn += 1
     print_board(board)
